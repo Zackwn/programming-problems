74f5333759a192d548752a959b703dfff0d80d01
Zackwn <rafaelfavazzi@gmail.com>
2020-10-14 21:42:41 -0300

    custom sort method for anagram

--- anagram.js
+++ anagram.js
@@ -2,7 +2,26 @@
 var query = ['abc', 'lock', 'qaui'];
 var dictionary = ['cba', 'abc', 'ckol', 'kolc', 'kcol', 'lolck', 'qauil'];
 var sort = function (target) {
-    return target.split('').sort().join('');
+    // return target.split('').sort().join('')
+    var targetArr = target.split('').map(function (letter) { return letter.toUpperCase(); });
+    var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    var result = '';
+    for (var ALPindex = 0; ALPindex < alphabet.length; ALPindex++) {
+        var letter = alphabet[ALPindex];
+        var indexesInTarget = [];
+        for (var i = 0; i < targetArr.length; i++) {
+            if (targetArr[i] === letter) {
+                indexesInTarget.push(i);
+            }
+        }
+        if (indexesInTarget.length === 0) {
+            continue;
+        }
+        indexesInTarget.forEach(function (index) {
+            result += target[index];
+        });
+    }
+    return result;
 };
 var solve = function (query, dictionary) {
     var result = {};
