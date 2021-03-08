2a0e35af09efca310ae8f64a48452050eaf3324d
Rafael Azzi <rafaelfavazzi@gmail.com>
2021-03-08 10:30:48 -0300

    search rotated sorted array

---
+++ searchRotatedSortedArray.go
@@ -0,0 +1,29 @@
+func search(nums []int, target int) int {
+	indexStart := 0
+	indexEnd := len(nums) - 1
+
+	for indexStart <= indexEnd {
+		mid := indexStart + (indexEnd-indexStart)/2
+
+		if nums[mid] == target {
+			return mid
+		}
+
+		if nums[indexStart] <= nums[mid] {
+			if target >= nums[indexStart] && target <= nums[mid] {
+				indexEnd = mid - 1
+			} else {
+				indexStart = mid + 1
+			}
+		} else {
+			// nums[indexStart] > nums[mid]
+			if target > nums[mid] && target <= nums[indexEnd] {
+				indexStart = mid + 1
+			} else {
+				indexEnd = mid - 1
+			}
+		}
+	}
+
+	return -1
+}
