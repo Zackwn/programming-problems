4fccacba50965f82174ec50f1412d6994eeefb61
Rafael Azzi <rafaelfavazzi@gmail.com>
2021-03-13 22:40:26 -0300

    atoi implementation

---
+++ myAtoi.go
@@ -0,0 +1,45 @@
+func myAtoi(s string) int {
+	isUnsigned := false
+	signAlreadyDefined := false
+	var integer int
+	for index, char := range s {
+		if char == ' ' {
+			if signAlreadyDefined {
+				break
+			}
+			continue
+		}
+		if char == '-' || char == '+' {
+			if signAlreadyDefined {
+				break
+			}
+			isUnsigned = char == '-'
+			signAlreadyDefined = true
+			continue
+		}
+		if unicode.IsDigit(char) {
+			for i := index; i < len(s); i++ {
+				if unicode.IsDigit(rune(s[i])) {
+					// '0' = 48
+					integer = integer*10 + int(rune(s[i])-'0')
+					if integer > math.MaxInt32 {
+						if isUnsigned {
+							return math.MinInt32
+						} else {
+							return math.MaxInt32
+						}
+					}
+				} else {
+					break
+				}
+			}
+			break
+		} else {
+			break
+		}
+	}
+	if isUnsigned {
+		integer *= -1
+	}
+	return integer
+}
