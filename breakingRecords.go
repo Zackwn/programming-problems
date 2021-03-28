f2962a86dac9ed8c19a428dae24c5cfa677d1700
Rafael Azzi <rafaelfavazzi@gmail.com>
2021-03-28 19:46:43 -0300

    breaking records

---
+++ breakingRecords.go
@@ -0,0 +1,19 @@
+func breakingRecords(scores []int32) []int32 {
+	var brokeBest int32 = 0
+	var brokeWorst int32 = 0
+	minScore := scores[0]
+	maxScore := scores[0]
+
+	for index := 1; index < len(scores); index++ {
+		score := scores[index]
+		if score > maxScore {
+			maxScore = score
+			brokeBest++
+		} else if score < minScore {
+			minScore = score
+			brokeWorst++
+		}
+	}
+
+	return []int32{brokeBest, brokeWorst}
+}
