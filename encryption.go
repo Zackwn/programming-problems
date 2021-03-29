9e3670b2a35682e1f5792261856da5ae62855d33
Rafael Azzi <rafaelfavazzi@gmail.com>
2021-03-29 13:35:08 -0300

    encryption solution

---
+++ encryption.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println(encryption("feedthedog")) // fto ehg ee dd
+}
+
+func encryption(s string) string {
+	s = deleteSpaces(s)
+	ssr := sqrt(float64(len(s)))
+	rows := int(math.Floor(ssr))
+	columns := int(math.Ceil(ssr))
+
+	if rows == columns {
+		rows--
+	}
+
+	trows := rows
+	if rows*columns < len(s) {
+		trows++
+	}
+
+	result := ""
+	for i := 0; i < columns; i++ {
+		r := 0
+		for k := 0; k < trows; k++ {
+			if k+r+i >= len(s) {
+				break
+			}
+			result += string(s[k+r+i])
+			r += rows
+		}
+		result += " "
+	}
+	return result
+}
+
+func sqrt(x float64) float64 {
+	var lz float64
+	z := 1.0
+	for i := 0; i < 1000; i++ {
+		lz = z
+		z -= (z*z - x) / (2 * z)
+		if lz == z {
+			return z
+		}
+	}
+	return z
+}
+
+func deleteSpaces(s string) string {
+	result := ""
+	for index := 0; index < len(s); index++ {
+		if s[index] != ' ' {
+			result += string(s[index])
+		}
+	}
+	return result
+}
