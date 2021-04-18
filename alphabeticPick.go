9ffa42352152791697fd0104245fe85679158544
Rafael Azzi <rafaelfavazzi@gmail.com>
2021-04-18 14:00:10 -0300

    alphabetic pick

---
+++ alphabeticPick.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(alphabeticPick("abdw", "abcgggg"))
+}
+
+func alphabeticPick(s1, s2 string) string {
+	if s1 == s2 {
+		return s1
+	}
+
+	i := 0
+	for i < len(s1) && i < len(s2) {
+		if s1[i] > s2[i] {
+			return s2
+		} else if s1[i] < s2[i] {
+			return s1
+		} else {
+			// s1[i] == s2[i]
+			i++
+		}
+	}
+
+	if len(s1) > len(s2) {
+		return s2
+	} else {
+		return s1
+	}
+}
