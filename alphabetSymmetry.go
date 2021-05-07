47cbffe002bd57353c2888c1bede44c6e27e3319
Rafael Azzi <rafaelfavazzi@gmail.com>
2021-05-06 23:57:00 -0300

    alphabet symmetry multithread

--- alphabetSymmetry.go
+++ alphabetSymmetry.go
@@ -14,3 +14,23 @@ func solve(slice []string) []int {
 	}
 	return r
 }
+
+func solveWithGoroutines(slice []string) []int {
+	// Your code here and happy coding!
+	r := make([]int, 0, len(slice))
+	rc := make(chan int)
+	for _, str := range slice {
+		go func(str string, rc chan<- int) {
+			t := 0
+			for position, char := range str {
+				if (int(char-97) == position) || (int(char-65) == position) {
+					t++
+				}
+			}
+			rc <- t
+		}(str, rc)
+		r = append(r, <-rc)
+	}
+
+	return r
+}
