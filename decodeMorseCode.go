febd67cbc855169724f49254232a9f3dbea83271
Rafael Azzi <rafaelfavazzi@gmail.com>
2021-05-08 00:14:44 -0300

    decode morse code

---
+++ decodeMorseCode.go
@@ -0,0 +1,29 @@
+package main
+
+func DecodeMorse(morseCode string) string {
+	result := ""
+	morse := []rune(morseCode)
+	for morse[0] == ' ' {
+		morse = morse[1:]
+	}
+	spacesInRow := 0
+	morseChar := ""
+	for i := 0; i < len(morse); i++ {
+		if morse[i] == ' ' {
+			spacesInRow++
+			continue
+		}
+		if spacesInRow != 0 {
+			result += MORSE_CODE[morseChar]
+			morseChar = string(morse[i])
+			if spacesInRow == 3 {
+				result += " "
+			}
+		} else {
+			morseChar += string(morse[i])
+		}
+		spacesInRow = 0
+	}
+	result += MORSE_CODE[morseChar]
+	return result
+}
