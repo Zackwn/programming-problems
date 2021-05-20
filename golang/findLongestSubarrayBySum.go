74d32007ddb5c04d361c6012be6989ab379e83b3
Rafael Azzi <rafaelfavazzi@gmail.com>
2021-05-20 14:47:03 -0300

    better solution (sliding window)

--- golang/findLongestSubarrayBySum.go
+++ golang/findLongestSubarrayBySum.go
@@ -1,5 +1,29 @@
 package main
 
+// sliding window algorithm
+func findLongestSubarrayBySumBetterSolution(s int, arr []int) []int {
+	rstart, rend := -1, -1
+	start, end := 0, 0
+	sum := 0
+	for end < len(arr) {
+		sum += arr[end]
+		for start < len(arr) && sum > s {
+			sum -= arr[start]
+			start++
+		}
+		if sum == s && start-end <= rstart-rend {
+			rstart = start
+			rend = end
+		}
+		end++
+	}
+	if rend < 0 {
+		return []int{-1}
+	}
+	return []int{rstart + 1, rend + 1}
+}
+
+// "brute force"
 func findLongestSubarrayBySum(s int, arr []int) []int {
 	result := []int{-1}
 	for i := 0; i < len(arr); i++ {
